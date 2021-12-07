41e5efc0a90a0937e2572b03a24201911e9c839c
Ivan <vanyok.korobka@ukr.net>
2021-12-07 22:31:45 +0200

    added a router functionally in App.js, added a router functional in app.js, pulled and placed the application pages in a separate folder - pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,44 +1,30 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+/** Когда мы указываем в пути только папку, без файла, 
+ * wabpack ищет файлик index.js с которого и подтягивает
+ * нужные компоненты 
+ */
+import {MainPage, ComicsPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
-
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
 
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                {/* <ErrorBoundary>
-                    <RandomChar />
-                </ErrorBoundary>
-
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundary>
-
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" /> */}
-                <AppBanner/>
-                <ComicsList/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage/>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 
 }
