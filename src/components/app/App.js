09f5feda0858e8013b208501ec7f53eb44c3c962
Ivan <vanyok.korobka@ukr.net>
2021-12-10 17:28:34 +0200

    added lazy function in App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,17 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 /** Когда мы указываем в пути только папку, без файла, 
  * wabpack ищет файлик index.js с которого и подтягивает
  * нужные компоненты 
  */
-import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
+import Spinner from '../spinner/Spinner';
 import AppHeader from "../appHeader/AppHeader";
+
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage.js'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage.js'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage.js'));
 
 const App = () => {
 
@@ -14,20 +20,22 @@ const App = () => {
             <div className="app">
                 <AppHeader />
                 <main>
-                    <Switch>
-                        <Route exact path="/">
-                            <MainPage/>
-                        </Route>
-                        <Route exact path="/comics">
-                            <ComicsPage/>
-                        </Route>
-                        <Route exact path="/comics/:comicId">
-                            <SingleComicPage/>
-                        </Route>
-                        <Route path="*">
-                            <Page404/>
-                        </Route>
-                    </Switch>
+                    <Suspense fallback={<Spinner/>}>
+                        <Switch>
+                            <Route exact path="/">
+                                <MainPage />
+                            </Route>
+                            <Route exact path="/comics">
+                                <ComicsPage />
+                            </Route>
+                            <Route exact path="/comics/:comicId">
+                                <SingleComicPage />
+                            </Route>
+                            <Route path="*">
+                                <Page404 />
+                            </Route>
+                        </Switch>
+                    </Suspense>
                 </main>
             </div>
         </Router>
